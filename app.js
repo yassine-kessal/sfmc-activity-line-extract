0a693e15f9f1e3e9e54b61da4d944bad6b480565
Yassine Kessal <yassine.kessal@lineup7.fr>
2022-05-03 18:42:04 +0200

    update

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const path = require('path');
 const activityConfig = require('./activity-config');
 const logger = require('./server/utils/logger');
 const sqlite3 = require('sqlite3').verbose();
+const { stringify } = require('csv-stringify');
+var fs = require('fs');
+
+var groupBy = function (xs, key) {
+    return xs.reduce(function (rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+    }, {});
+};
 
 const db = new sqlite3.Database('./database.sqlite3');
 
@@ -109,6 +118,64 @@ app.get('/get-data', function (req, res) {
             }
         );
     });
+});
+
+app.get('/generate', function (req, res) {
+    // TODO for now 0s, after change to 900s = 15m
+    db.serialize(function () {
+        db.all(
+            `SELECT fields.name, fields.value, files.activityId, fields.defId FROM fields
+        INNER JOIN files ON files.activityId = fields.fileActivityId`,
+            function (err, rows) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                console.log(rows);
+
+                let groupedRows = groupBy(rows, 'defId'),
+                    formattedRows = [];
+
+                Object.keys(groupedRows).forEach((keyRow) => {
+                    let v = {};
+                    groupedRows[keyRow].forEach((row) => {
+                        v[row.name] = row.value;
+                    });
+                    formattedRows.push(v);
+                });
+
+                console.log(formattedRows);
+
+                stringify(
+                    formattedRows,
+                    {
+                        header: true,
+                        delimiter: ';'
+                    },
+                    function (err, data) {
+                        if (err) {
+                            console.log(err);
+                            return;
+                        }
+
+                        console.log(data);
+                        fs.writeFile(
+                            __dirname + '/exported/test.csv',
+                            data,
+                            (err, data) => {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            }
+                        );
+                    }
+                );
+            }
+        );
+    });
+
+    return res.status(200).json({});
 });
 
 if (process.env.NODE_ENV !== 'production') {
